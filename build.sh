c9a99feeaa97cc4f4446220444710e72c22b500b
tomo-chan <aradgyma@gmail.com>
2019-03-03 04:35:49 +0900

    bundlerをインストール

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -ue
 
-# ./asciidoctor.sh -D dist src/index.adoc
-asciidoctor -a attribute-missing=warn --failure-level=WARN -D dist src/index.adoc
-cp -r src/images dist
+# ./asciidoctor.sh -D gh-pages src/index.adoc
+asciidoctor -a attribute-missing=warn --failure-level=WARN -D gh-pages src/index.adoc
+cp -r src/images gh-pages
 
-asciidoctor-pdf -r asciidoctor-pdf-cjk-kai_gen_gothic -D dist src/index.adoc
+asciidoctor-pdf -r asciidoctor-pdf-cjk-kai_gen_gothic -D gh-pages src/index.adoc
